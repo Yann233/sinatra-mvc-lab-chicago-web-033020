dac8b410cc71532e785621053a34b3cd1455f028
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-22 15:52:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,11 +3,9 @@ class PigLatinizer
   attr_reader :text
 
   def piglatin(word)
-   if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
-   elsif word =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'ay'
-   end
-word
-end
+    if word[/\A[aeiou]/i]
+      word + 'ay'
+    else
+      word[1..-1] + word[0] + 'ay'
+    end
+  end
