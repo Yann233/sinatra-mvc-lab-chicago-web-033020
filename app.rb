15b7775e813919a1ef32654b1daf47f43859f533
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-22 15:18:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
+# Create a POST method in your controller (app.rb) to receive your form's params.
+
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @analyzed_words = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :piglatinize
+  end
+
 end
